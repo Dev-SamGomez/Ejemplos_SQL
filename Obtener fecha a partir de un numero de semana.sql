a28eaeb68c7602ca356941835b57cf1c5d0fc891
Samuel Gomez <106413475+Dev-SamGomez@users.noreply.github.com>
2022-06-06 13:48:11 -0600

    Add files via upload

--- Obtener fecha a partir de un numero de semana.sql
+++ Obtener fecha a partir de un numero de semana.sql
@@ -1,3 +1,4 @@
 declare @año int =2022,
 @semana int =23,@dia int =2
 declare @inicioaño datetime=cast(cast( @año*10000+101 as varchar) as datetime)
+select dateadd(day,(@semana)*7+@dia-DATEPART(WEEKDAY,@inicioaño),@inicioaño)
